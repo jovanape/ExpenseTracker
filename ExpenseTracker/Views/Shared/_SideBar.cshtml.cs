05a376629f524d9a4b40d8a526a13187a516d217
Pejkic Jovana TH354 <Jovana.Pejkic@draexlmaier.com>
2024-06-27 21:30:00 +0200

    Updates.

---
+++ ExpenseTracker/Views/Shared/_SideBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExpenseTracker.Views.Shared
+{
+    public class _SideBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
